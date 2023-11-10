85a6a327dc594a41255adde224516fcd3e597330
Lucas Israel <lisraelalves92@gmail.com>
2023-11-10 16:42:53 -0300

    exposing post /persons and post /auth/login

--- src/main/java/com/betrybe/agrix/security/SecurityConfiguration.java
+++ src/main/java/com/betrybe/agrix/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.betrybe.agrix.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,6 +18,11 @@ public class SecurityConfiguration {
     return httpSecurity
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests( authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/persons").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .anyRequest().authenticated()
+        )
         .build();
   }
 
