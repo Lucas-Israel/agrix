57d774d789f4c43d96a886f4da3547075a9d6908
Lucas Israel <lisraelalves92@gmail.com>
2023-11-10 16:36:13 -0300

    added: authentication controller

---
+++ src/main/java/com/betrybe/agrix/controllers/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.betrybe.agrix.controllers;
+
+import com.betrybe.agrix.controllers.dto.AuthenticationDto;
+import com.betrybe.agrix.models.entity.Person;
+import com.betrybe.agrix.services.PersonService;
+import com.betrybe.agrix.services.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+  private final AuthenticationManager authenticationManager;
+
+  private final TokenService tokenService;
+
+  @Autowired
+  public AuthenticationController(AuthenticationManager authenticationManager,
+      TokenService tokenService) {
+    this.authenticationManager = authenticationManager;
+    this.tokenService = tokenService;
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestBody AuthenticationDto authenticationDto) {
+    UsernamePasswordAuthenticationToken userPassword =
+        new UsernamePasswordAuthenticationToken(
+            authenticationDto.username(),
+            authenticationDto.password()
+        );
+    Authentication auth = authenticationManager.authenticate(userPassword);
+    Person person = (Person) auth.getPrincipal();
+    String token = tokenService.generateToken(person);
+    return ResponseEntity.ok(token);
+  }
+}
